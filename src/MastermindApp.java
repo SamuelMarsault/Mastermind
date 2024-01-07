d6d227f1d550f6620a96a113c868584058ab22c4
Samuel <marsaults21@gmail.com>
2024-01-07 17:56:25 +0100

    Interface Theme changement

--- src/MastermindApp.java
+++ src/MastermindApp.java
@@ -3,25 +3,36 @@ import controler.RoundController;
 import model.*;
 import view.*;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class MastermindApp {
     public static void main(String[] args) {
-        Game game = new Game();
+        try {            
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            SwingUtilities.updateComponentTreeUI(new JFrame());
+            
+            Game game = new Game();
 
-        RoundController roundController = new RoundController();
-        GameController gameController = new GameController(roundController, game);
+            RoundController roundController = new RoundController();
+            GameController gameController = new GameController(roundController, game);
 
-        StartPanel startPanel = new StartPanel(gameController);
-        GamePanel gamePanel = new GamePanel(roundController,gameController);
-        EndPanel endPanel = new EndPanel(gameController);
-        MastermindWindow window = new MastermindWindow(endPanel,gamePanel,startPanel);
+            StartPanel startPanel = new StartPanel(gameController);
+            GamePanel gamePanel = new GamePanel(roundController, gameController);
+            EndPanel endPanel = new EndPanel(gameController);
 
-        gameController.setWindow(window);
-        roundController.setWindow(window);
-        roundController.setGamePanel(gamePanel);
+            MastermindWindow window = new MastermindWindow(endPanel, gamePanel, startPanel);
 
-        game.addObserver(gamePanel);
-        game.addObserver(endPanel);
+            gameController.setWindow(window);
+            roundController.setWindow(window);
+            roundController.setGamePanel(gamePanel);
+
+            game.addObserver(gamePanel);
+            game.addObserver(endPanel);
+
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            // Gérer l'exception de manière appropriée
+        }
     }
 }
--- src/view/MastermindWindow.java
+++ src/view/MastermindWindow.java
@@ -11,32 +11,37 @@ public class MastermindWindow extends JFrame {
     private StartPanel startPanel;
     private GamePanel gamePanel;
     private EndPanel endPanel;
-    public MastermindWindow(EndPanel endPanel, GamePanel gamePanel, StartPanel startPanel){
+
+    public MastermindWindow(EndPanel endPanel, GamePanel gamePanel, StartPanel startPanel) {
         super("Mastermind");
+
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setSize( 700, (int)screenSize.getHeight()-50 );
-        setLocation((int)(screenSize.getWidth()/4),0);
+        setSize(700, (int) screenSize.getHeight() - 50);
+        setLocation((int) (screenSize.getWidth() / 4), 0);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         this.startPanel = startPanel;
         this.gamePanel = gamePanel;
         this.endPanel = endPanel;
+
         add(startPanel);
         setVisible(true);
     }
 
-    public void showStartPanel(){
+    public void showStartPanel() {
         startPanel.setVisible(true);
         gamePanel.setVisible(false);
         endPanel.setVisible(false);
     }
 
-    public void showGamePanel(){
+    public void showGamePanel() {
         add(gamePanel);
         startPanel.setVisible(false);
         gamePanel.setVisible(true);
         endPanel.setVisible(false);
     }
-    public void showEndPanel(){
+
+    public void showEndPanel() {
         add(endPanel);
         startPanel.setVisible(false);
         gamePanel.setVisible(false);
